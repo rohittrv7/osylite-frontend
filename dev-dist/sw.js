c56f47171e3fac84dd6f6fe805f111eed7deeb6d
rohitrv7 <rohitkumar115032002@gmail.com>
2026-05-13 21:33:33 +0530

    add agent service

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.og73ccoshqk"
+    "revision": "0.rsi3ep7i25c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
