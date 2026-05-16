59598a6d16d512018cfbf6b81c9e8c13b9ea6207
rohitrv7 <rohitkumar115032002@gmail.com>
2026-05-16 12:26:37 +0530

    add code

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rsi3ep7i25c"
+    "revision": "0.05n0trmmrp4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
