f5edd46491fc11af5ca1d492adbcc7e25b4e233c
agrani Kumar <158788185+Agrani-kumar-01@users.noreply.github.com>
2026-05-16 09:02:55 +0530

    Update base URL for API requests

--- src/store/api/rootApiSlice.ts
+++ src/store/api/rootApiSlice.ts
@@ -12,7 +12,7 @@ import { env } from "@/config/env";
 const mutex = new Mutex();
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: env.VITE_BACKEND_BASE_URL,
+  baseUrl: "https://api.osylite.com/api",
 
   credentials: "include",
 
